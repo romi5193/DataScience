339c0d70899cfd70ed9772c750564ea578b81f00
Romaine Isaacs <romaineisaacs@Romaine-iMac.local>
2019-05-22 22:17:57 -0400

    cleaned

--- Web_Scraping/URL_fetch/find_word.py
+++ Web_Scraping/URL_fetch/find_word.py
@@ -1,16 +1,16 @@
+#This script finds the number of times an author uses a word in an html book
+
 import urllib3
-test = raw_input("What word do you want to know the frequency of?...")
-#url=raw_input("What is the url of the html text?...")
-instance=urllib3.PoolManager()
-responce=instance.request('GET', 'http://www.gutenberg.org/files/135/135-h/135-h.htm')
-html=str(responce.data)
+target_word = input(str("What word do you want to know the frequency of?..."))
+url=input(str("What is the url of the html text?..."))
 
-known_word=0
-list_of_words= html.split(' ')
-#print(list_of_words)
+instance = urllib3.PoolManager()
+responce = instance.request('GET', url)
+html = str(responce.data)
 
+known_word = 0
+list_of_words = html.split(' ')
 for word in list_of_words:
-    if word==str(test):
+    if word == str(target_word):
         known_word += 1
-print('The word %s occurs %d times' %(test, known_word))
-#print(known_word)
+print('The word %s occurs %d times' %(target_word, known_word))
