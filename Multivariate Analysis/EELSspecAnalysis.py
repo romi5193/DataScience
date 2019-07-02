955a57fcbae47a24ca7957650f546f696d7e3d53
Romaine Isaacs <romaineisaacs@Romaine-iMac.local>
2019-07-01 22:17:27 -0400

    Multivariate and README added

---
+++ Multivariate Analysis/EELSspecAnalysis.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import hyperspy.api as hs
+import matplotlib.pyplot as plt
+
+
+
+im = hs.load('EELS Spectrum Image 1.dm3')
+im.plot()
+
+im.spikes_removal_tool()
+im.remove_background()
+
+#isolate Carbon_k-edge
+s_C=im.isig[250.0:340.0].deepcopy()
+s_C.plot()
+
+#determine the number of components using Singular Value Decomposition algorithm
+s_C.decomposition(False, algorithm='svd')
+s_C.plot_explained_variance_ratio()
+s_C.get_decomposition_model(components=5).plot()
+s_C.plot_decomposition_results()
+
+#separate mutually independent components from a mixture using Blind Source Separation algorithm
+s_C.blind_source_separation(number_of_components=5)
+s_C.plot_bss_results()
+s_C.reverse_bss_component(2)
+s_C.reverse_bss_component(1)
+s_C.plot_bss_results()
+
+#plot and save final results
+utils.plot.plot_images([i for i in s_C.get_bss_loadings()], cmap='cubehelix',
+                       per_row=2,
+                       colorbar='multi',
+                       axes_decor=None,
+                       label='auto',
+                       scalebar=[0],
+                       scalebar_color='black')
+
+utils.plot.plot_spectra(s_C.get_bss_factors(),
+                        style='cascade',legend=None,legend_loc=0)
+s_C.save("EELS_SI4_CKedge_SVD5.hdf5")
+plt.close('all')
---
+++ Multivariate Analysis/README.md
@@ -0,0 +1,9 @@
+# Multivariate analysis of Electron Energy Loss Spectroscopy data
+
+Electron Energy Loss Spectroscopy (EELS) is a critical tool for chemical analysis of materials. The EELS spectrum is a plot of the energy loss vs. intensity. *EELS Spectrum Image 1.dm3* is a 3 dimensional dataset (the area scanned in x and y with an EELS spectra acquired at each pixel).
+
+- *EELS Spectrum Image 1.dm3* is an example dataset
+- *EELSspecanalysis.py* is the analysis of this data
+- Results consist of:
+  - *factors.png* (deconvoluted spectra with each component identified)
+  - *loadings.png* (maps of each component)
